80f54b7ba99aa9d85c722ef808b488cfc5d683f2
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-13 21:08:15 +0530

    add icons in splash screen

--- lib/Screen/view/splash_screen/splash_screen.dart
+++ lib/Screen/view/splash_screen/splash_screen.dart
@@ -9,19 +9,35 @@ class ScreenSplash extends StatelessWidget {
   ScreenSplash({super.key});
 
   final splashC = Get.put(SplashContorller());
+  final width = Get.size.width;
 
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       splashC.splashTimer();
     });
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
           child: Center(
-        child: Image(
-            image: AssetImage(
-                'assets/images/Screenshot_2023-01-13-20-39-39-93_40deb401b9ffe8e1df2f1cc5ba480b12.jpg')),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage(
+                  'assets/images/Screenshot_2023-01-13-20-39-39-93_40deb401b9ffe8e1df2f1cc5ba480b12.jpg'),
+            ),
+            Text(
+              'OJOURN',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 5, 53, 92),
+                fontWeight: FontWeight.bold,
+                fontSize: width * 0.15,
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
