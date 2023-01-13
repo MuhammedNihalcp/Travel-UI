a3d03eaafea7f696b1f5f16b7dbae9f116ff0ca5
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-13 20:49:27 +0530

    add icon image

---
+++ lib/Screen/view/splash_screen/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:travel_ui/core/const/colors.dart';
+
+class ScreenSplash extends StatelessWidget {
+  const ScreenSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+    body: SafeArea(child: Center()),);
+  }
+}
